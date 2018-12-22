b4c3f3c1a87931e6274b114982a43c3fa570ce07
Gyiming <yiminggan@umail.ucsb.edu>
2018-12-22 17:33:19 -0500

    multiprocessing

--- set_cpu.py
+++ set_cpu.py
@@ -1,13 +1,15 @@
 from __future__ import with_statement
 from multiprocessing import Process
+import multiprocessing
 import os
 import configparser
 import random
 import numpy as np 
-import timeit
+import time
+import math
 
 def agent():
-
+    return 0
 
 def governer():
 	i =0
@@ -32,24 +34,36 @@ def governer():
 
 def run_benchmark():
 	execute_command = 'parsecmgmt -a run -p all'
-	os.system(execute_command)
+	#os.system(execute_command)
+	print execute_command	
 
 def power_measure_tx2():
-	f = open("/sys/devices/316000.i2c/i2c-0/0-0041/iio_device/in_power1_input","r")
-	cpu_power = f.read()
+	#f = open("/sys/devices/316000.i2c/i2c-0/0-0041/iio_device/in_power1_input","r")
+	#cpu_power = f.read()
+	print('150mw')
 
 def main():
-	start = timeit.timeit()
+	start_p1 = time.time()
+	start_p2 = time.time()
+	start_p3 = time.time()
 	while True:
 		p1 = multiprocessing.Process(target=run_benchmark)
 		p2 = multiprocessing.Process(target=governer)
 		p3 = multiprocessing.Process(target=power_measure_tx2)
-		p1.start()
-		if (timeit.timeit() - start) % 100 == 0:
+		accumulate_p1 = time.time() - start_p1
+		accumulate_p2 = time.time() - start_p2
+		accumulate_p3 = time.time() - start_p3
+		if accumulate_p1 > 5:
+			p1.start()
+			start_p1 = time.time()
+		if accumulate_p2 > 3:
 			p2.start()
-
-		if (timeit.timeit() - start) % 10000 == 0:
+			#print math.floor((time.time() - start_p2))
+			start_p2 = time.time()
+		if accumulate_p3 > 10:
 			p3.start()
+			#print math.floor((time.time() - start_p3))
+                        start_p3 = time.time()
 
 
 if __name__ == '__main__':
